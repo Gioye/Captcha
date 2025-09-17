4fbac717d63640f4a8f6077f27d4bf9328b8e235
Gioye <100699274+Gioye@users.noreply.github.com>
2025-09-17 21:02:00 +0200

    Update captcha.js

--- functions/captcha.js
+++ functions/captcha.js
@@ -1,6 +1,18 @@
+const https = require('https');
+
 exports.handler = async function () {
   const num = Math.floor(Math.random() * 288) + 1;
   const code = num.toString().padStart(3, '0');
+  const imageUrl = `https://wowcaptcha.netlify.app/captchas/${code}.png`;
+
+  const imageBuffer = await new Promise((resolve, reject) => {
+    https.get(imageUrl, (res) => {
+      const data = [];
+      res.on('data', (chunk) => data.push(chunk));
+      res.on('end', () => resolve(Buffer.concat(data)));
+      res.on('error', reject);
+    }).on('error', reject);
+  });
 
   return {
     statusCode: 200,
@@ -8,7 +20,7 @@ exports.handler = async function () {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      captcha: `https://wowcaptcha.netlify.app/captchas/${code}.png`,
+      captcha: `data:image/png;base64,${imageBuffer.toString('base64')}`,
       correct: code
     })
   };
